74a1c7cbddf307db0cb0442fed0cb52a1fd31fc6
JamesZam96 <jameszamgm@gmail.com>
2024-06-18 15:29:33 -0500

    RegisterAndLogin

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware('guest')->group(function(){
-    Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register.form');
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
-    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form');
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::middleware('web')->group(function(){
+
+    Route::middleware('guest')->group(function(){
+        Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register.form');
+        Route::post('/register', [AuthController::class, 'register'])->name('register');
+        Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form');
+        Route::post('/login', [AuthController::class, 'login'])->name('login'); 
+    });
+
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware('auth')->name('dashboard');
+
 });
-
-
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware('auth')->name('dashboard');
