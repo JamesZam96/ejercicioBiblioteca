f12cf2569911d1a5db58457c2ce8f00ae626c8a6
JamesZam96 <jameszamgm@gmail.com>
2024-06-19 10:36:49 -0500

    create theme

--- resources/views/themes/index.blade.php
+++ resources/views/themes/index.blade.php
@@ -1,20 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<!--<div>
-    <h1>Temas</h1>
-    <a href="{{ route('themes.create') }}">Create Product</a>
-    <ul>
-        @foreach ($themes as $theme)
-            <li>{{ $theme->name }} - {{ $theme->codeColor }}</li>
-        @endforeach
-    </ul>
-</div>-->
 <div class="container mt-5">
         <div class="row">
             <div class="col-12">
                 <h1 class="mb-4">Temas</h1>
-                <a href="{{ route('themes.create') }}" class="btn btn-primary mb-3">Create Product</a>
+                <a href="{{ route('themes.create') }}" class="btn btn-primary mb-3">Crear tema</a>
                 <ul class="list-group">
                     @foreach ($themes as $theme)
                         <li class="list-group-item d-flex justify-content-between align-items-center">
